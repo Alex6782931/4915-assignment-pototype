b478a12d4d122f060937bfab37a12db3f048ce05
stcp <stcp@STITN50535>
2026-06-11 14:08:00 +0800

    dawda

--- 4915 assignment pototype/OrderMainPage.Designer.cs
+++ 4915 assignment pototype/OrderMainPage.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            dataGridView1 = new DataGridView();
-            textBox1 = new TextBox();
-            label2 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            dataOrder = new DataGridView();
+            txtbOsearch = new TextBox();
+            lblOsearch = new Label();
+            btnOsearch = new Button();
+            btnOview = new Button();
+            btnOdelete = new Button();
+            btnOcae = new Button();
+            btnoback = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataOrder).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -49,92 +49,92 @@
             label1.TabIndex = 0;
             label1.Text = "Order";
             // 
-            // dataGridView1
+            // dataOrder
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(71, 108);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(643, 238);
-            dataGridView1.TabIndex = 1;
+            dataOrder.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataOrder.Location = new Point(71, 108);
+            dataOrder.Name = "dataOrder";
+            dataOrder.Size = new Size(643, 238);
+            dataOrder.TabIndex = 1;
             // 
-            // textBox1
+            // txtbOsearch
             // 
-            textBox1.Location = new Point(207, 67);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(217, 23);
-            textBox1.TabIndex = 2;
+            txtbOsearch.Location = new Point(207, 67);
+            txtbOsearch.Name = "txtbOsearch";
+            txtbOsearch.Size = new Size(217, 23);
+            txtbOsearch.TabIndex = 2;
             // 
-            // label2
+            // lblOsearch
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(123, 74);
-            label2.Name = "label2";
-            label2.Size = new Size(38, 15);
-            label2.TabIndex = 3;
-            label2.Text = "label2";
+            lblOsearch.AutoSize = true;
+            lblOsearch.Location = new Point(123, 74);
+            lblOsearch.Name = "lblOsearch";
+            lblOsearch.Size = new Size(63, 15);
+            lblOsearch.TabIndex = 3;
+            lblOsearch.Text = "Search for:";
             // 
-            // button1
+            // btnOsearch
             // 
-            button1.Location = new Point(466, 66);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 4;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnOsearch.Location = new Point(446, 66);
+            btnOsearch.Name = "btnOsearch";
+            btnOsearch.Size = new Size(75, 23);
+            btnOsearch.TabIndex = 4;
+            btnOsearch.Text = "Search";
+            btnOsearch.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnOview
             // 
-            button2.Location = new Point(639, 370);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 5;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btnOview.Location = new Point(639, 370);
+            btnOview.Name = "btnOview";
+            btnOview.Size = new Size(75, 23);
+            btnOview.TabIndex = 5;
+            btnOview.Text = "View";
+            btnOview.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnOdelete
             // 
-            button3.Location = new Point(553, 370);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 6;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            btnOdelete.Location = new Point(541, 370);
+            btnOdelete.Name = "btnOdelete";
+            btnOdelete.Size = new Size(75, 23);
+            btnOdelete.TabIndex = 6;
+            btnOdelete.Text = "Delete";
+            btnOdelete.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnOcae
             // 
-            button4.Location = new Point(446, 370);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 23);
-            button4.TabIndex = 7;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            btnOcae.Location = new Point(415, 370);
+            btnOcae.Name = "btnOcae";
+            btnOcae.Size = new Size(106, 23);
+            btnOcae.TabIndex = 7;
+            btnOcae.Text = "Create and edit";
+            btnOcae.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnoback
             // 
-            button5.Location = new Point(12, 9);
-            button5.Name = "button5";
-            button5.Size = new Size(60, 28);
-            button5.TabIndex = 8;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
+            btnoback.Location = new Point(12, 9);
+            btnoback.Name = "btnoback";
+            btnoback.Size = new Size(60, 28);
+            btnoback.TabIndex = 8;
+            btnoback.Text = "back";
+            btnoback.UseVisualStyleBackColor = true;
             // 
             // OrderMainPage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(label2);
-            Controls.Add(textBox1);
-            Controls.Add(dataGridView1);
+            Controls.Add(btnoback);
+            Controls.Add(btnOcae);
+            Controls.Add(btnOdelete);
+            Controls.Add(btnOview);
+            Controls.Add(btnOsearch);
+            Controls.Add(lblOsearch);
+            Controls.Add(txtbOsearch);
+            Controls.Add(dataOrder);
             Controls.Add(label1);
             Name = "OrderMainPage";
             Text = "OrderMainPage";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataOrder).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -142,13 +142,13 @@
         #endregion
 
         private Label label1;
-        private DataGridView dataGridView1;
-        private TextBox textBox1;
-        private Label label2;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
+        private DataGridView dataOrder;
+        private TextBox txtbOsearch;
+        private Label lblOsearch;
+        private Button btnOsearch;
+        private Button btnOview;
+        private Button btnOdelete;
+        private Button btnOcae;
+        private Button btnoback;
     }
 }

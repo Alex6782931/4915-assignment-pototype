0f98ab66d3f45b4d5dafbdefc56646afb087cf93
dstcp <stcp@STITN50819>
2026-06-11 16:49:42 +0800

    dsadadas

---
+++ DatabaseAccessController/GetCompanyData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DatabaseAccessController
+{
+    public class GetCompanyData : DatabaseController
+    {
+        public GetCompanyData(string connectionString) : base(connectionString) { }
+    
+    public DataTable GetAllCustomerData()
+        {
+            String sqlCmd = "SELECT * FROM company";
+            return base.GetData(sqlCmd);
+        }
+    }
+}
